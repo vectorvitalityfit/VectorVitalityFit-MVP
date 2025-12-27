561503cca5f1b1fbb6ffc6175d524a57ed9d0c71
Sheraz Arshad <vectorvitalityfit@gmail.com>
2025-12-27 11:41:37 -0500

    Update deploy_ml.py

--- azure_ml/deploy_ml.py
+++ azure_ml/deploy_ml.py
@@ -5,13 +5,8 @@ from azure.ai.ml.entities import ManagedOnlineEndpoint
 from azure.core.exceptions import HttpResponseError
 
 def deploy_model():
-    subscription_id = "7cd27f16-62c4-43e9-8709-f1ea88ede961"
     resource_group = "VVF-Resources"
     workspace = "VVF-Workspace"
-
-    tenant_id = "95653864-218a-499e-9e25-a2649f81cb0b"
-    client_id = "9ce382bf-5f31-4f10-a2d5-e670db6f367b"
-    client_secret = "***REMOVED***"
 
     credential = ClientSecretCredential(
         tenant_id=tenant_id,
